7a50ac44770f2088761585193fde5a1d3623e972
davo <davitantonyan21@gmail.com>
2026-03-19 19:37:28 +0400

    iUpdateHomeworkPrimeCheck

---
+++ homework3(checkPrime).js
@@ -0,0 +1,16 @@
+function checkPrime (number) {
+    let cycle = 2;
+    
+    while (cycle < number / 2) {
+        if (number % cycle === 0) {
+            return false;
+        }
+        cycle++
+    }
+    return true;
+}
+
+console.log("Expexted: true, Result: " + checkPrime(7));
+console.log("Expexted: true, Result: " + checkPrime(17));
+console.log("Expexted: false, Result: " + checkPrime(26));
+console.log("Expexted: false, Result: " + checkPrime(17*13));
