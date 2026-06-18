ed3f320da1038ab4477495ae728b31c9f8a3da59
davo <davitantonyan21@gmail.com>
2026-06-18 14:22:55 +0400

    I created a new folder for June and wrote the Caesar Cipher assignment.

---
+++ 06.2026/caesar-cipher.js
@@ -0,0 +1,40 @@
+function encode(text, shift) {
+    let result = ""
+    for (let cycle = 0; cycle < text.length; cycle++) {
+        let code = text[cycle].charCodeAt(0);
+        if (code >= 97 && code <= 122 || code >= 65 && code <= 90) {
+            if (code >= 97 && code <= 122) {
+                let newCode = code + shift
+                if (newCode > 122) {
+                    newCode = newCode - 26
+                }
+                let newChar = String.fromCharCode(newCode );
+                result = result + newChar;
+            }
+            
+            if (code >= 65 && code <= 90) {
+                let newCode = code + shift
+                if (newCode > 90) {
+                    newCode = newCode - 26
+                }
+                let newChar = String.fromCharCode(newCode );
+                result = result + newChar;
+            }
+        } else {
+            let newChar = String.fromCharCode(code);
+            result = result + newChar;
+        }
+    }
+    
+    return result
+}
+
+function decode (text, shift) {
+    return encode(text, -shift);
+} 
+
+
+console.log(encode("Hello World!", 3))//  →  "Khoor Zruog!"
+console.log(decode("Khoor Zruog!", 3))//  →  "Hello World!"
+console.log(encode("xyz", 2))//           →  "zab"
+console.log(encode("ABC", 26))//          →  "ABC"
