7ee359f1e02fa69e8dadc996d9ddfec96b95641f
davo <davitantonyan21@gmail.com>
2026-06-19 18:19:55 +0400

    i edit my homework caesar-chiper to make it better

--- 06.2026/caesar-cipher.js
+++ 06.2026/caesar-cipher.js
@@ -7,19 +7,22 @@ function encode(text, shift) {
                 let newCode = code + shift
                 if (newCode > 122) {
                     newCode = newCode - 26
+                } else if (newCode < 97) {
+                    newCode = newCode + 26
                 }
                 let newChar = String.fromCharCode(newCode );
                 result = result + newChar;
             }
-            
             if (code >= 65 && code <= 90) {
                 let newCode = code + shift
                 if (newCode > 90) {
                     newCode = newCode - 26
+                } else if (newCode < 65) {
+                    newCode = newCode + 26
                 }
                 let newChar = String.fromCharCode(newCode );
                 result = result + newChar;
-            }
+            } 
         } else {
             let newChar = String.fromCharCode(code);
             result = result + newChar;
@@ -38,3 +41,5 @@ console.log(encode("Hello World!", 3))//  →  "Khoor Zruog!"
 console.log(decode("Khoor Zruog!", 3))//  →  "Hello World!"
 console.log(encode("xyz", 2))//           →  "zab"
 console.log(encode("ABC", 26))//          →  "ABC"
+console.log(decode("abc", 5))
+console.log(encode("XYZ", 26));
