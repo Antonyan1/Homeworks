5b801729ec58e4abd20d48d61efaffdb9a43fa86
davo <davitantonyan21@gmail.com>
2026-07-02 17:02:38 +0400

    working progress

--- 06.2026/runLengthEncoding.js
+++ 06.2026/runLengthEncoding.js
@@ -18,14 +18,25 @@ function compress(str) {
 
 function decompress(str) {
     let result = "";
+    let numBox = 1;
     for (let index = 0; index < str.length; index = index + 2) {
+        for (true) {
+            if (str[index] === str[index + 1]) {
+                numBox++
+            } else {break}
+        }
         let numCycle = Number(str[index]);
         let letter = str[index + 1];
-
-        result = result + letter.repeat(numCycle);
+        for (let numQuantity = 0; numQuantity < numBox; numQuantity++) {
+            
+        }
     }
     return result;
 }
 
 console.log(compress("aaabbc"));
-console.log(decompress("3a2b1c"));
+console.log(decompress("34a2b1c"));
+
+let compressed = compress("aaaaaaaaaaaa"); // 12 букв 'a'
+console.log(compressed);
+console.log(decompress(compressed));
