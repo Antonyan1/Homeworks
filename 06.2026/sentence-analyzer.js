df5c6ecde8b98b6f5ccd67f3ab618d2e71b00a4c
davo <davitantonyan21@gmail.com>
2026-07-02 18:15:38 +0400

    i finish my homework sentence-analyzer

--- 06.2026/sentence-analyzer.js
+++ 06.2026/sentence-analyzer.js
@@ -1,21 +1,20 @@
 function analyze(sentence) {
-    let result = [];
+    let result = {}
 
     let words = sentence.split(" ");
     let wordCount = words.length;
-
-    result = result + "wordsCount: " + wordCount + "\n";
+    result.wordsCount = wordCount;
     
     let charCount = sentence.replace(/ /g, "");
-    result = result + "charCount: " + charCount.length + "\n";
+    result.charCount = charCount.length;
     
     let counter = {};
     for (let word of words) {
         word = word.toLowerCase();
         if (counter[word] !== undefined) {
-                counter[word]++
+            counter[word]++
         } else {
-                counter[word] = 1;
+            counter[word] = 1;
         }
     }
     let mostFrequent = "";
@@ -27,30 +26,34 @@ function analyze(sentence) {
             mostFrequent = key;
         }   
     }
-    result = result + "mostFrequent: " + mostFrequent + "\n";
+    result.mostFrequent = mostFrequent;
 
     let reversed = "";
     for (let index = words.length - 1; index >= 0; index--) {
-        reversed = reversed + words[index] + " ";
+        if (index !== 0) {
+            reversed = reversed + words[index] + " ";
+        } else {
+            reversed = reversed + words[index];
+        }
     }
-    result = result + "reversed: " + reversed + "\n"
+    result.reversed = reversed;
     
     let text = sentence.toLowerCase();
     text = text.replace(/ /g, "");
     let letters = text.split("");
     
-    let isPalindrome;
+    let isPalindrome = "";
     for (let index = letters.length - 1; index >= 0; index--) {
         isPalindrome = isPalindrome + letters[index];
-    } 
+    }
     if (isPalindrome === text) {
         isPalindrome = true;
-    } else {
+    } else if (isPalindrome !== text) {
         isPalindrome = false;
     }
-    result = result + "isPalindrome: " + isPalindrome;
-
+    result.isPalindrome =  isPalindrome;
     return result;
 }
 
 console.log (analyze("to be or not to be"));
+console.log(analyze("never odd or even"))
